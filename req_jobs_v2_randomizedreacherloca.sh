4d434cfe1237e2cbb1e6ceb33b1a0def620bf3b5
Ruis MacDonald <rs660553@dal.ca>
2026-02-14 00:35:50 -0400

    fixed mem val

--- req_jobs_v2_randomizedreacherloca.sh
+++ req_jobs_v2_randomizedreacherloca.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --exclude=ng[11105,30708]
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --array=0
 #SBATCH --acctg-freq=task=1
 #SBATCH --output=/home/ruism/projects/def-rsdjjana/ruism/Dreamer_v2_SDH_fresh/results/randomizedreacherloca_v2_state_dist/%A-%a.out
